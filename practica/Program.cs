66d69a109ad4983b6f2630676cd0807d9be23f5b
OmarZamorano <126824615+OmarZamorano@users.noreply.github.com>
2023-03-13 22:18:36 +0000

    Actualizado

---
+++ practica/Program.cs
@@ -0,0 +1,34 @@
+﻿class Domino
+{
+    private int l1;
+    private int l2;
+    int total;
+    public Domino (int l1, int l2)
+    {
+        this.l1=l1;
+        this.l2=l2;
+    }
+
+    public void Imprime ()
+    {
+        Console.WriteLine((l1+l2));
+    }
+
+    public static Domino operator + (Domino d1,Domino d2)
+    {
+        int l1= d1.l1 + d1.l2;
+        int l2=d2.l1 + d2.l2;
+        return new Domino(l1,l2);
+    }
+}
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Domino d1 = new Domino (0,1);
+        Domino d2 = new Domino (2,3);
+        Domino total= d1 + d2;
+        total.Imprime();
+    }
+}
